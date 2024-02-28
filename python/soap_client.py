eadfae2818029ffb3aef6bcce683b1f7c4254239
WMinerva <nicolas.ochoa1697@gmail.com>
2024-02-28 02:08:09 -0400

    tarea SOAP sumar y palindromo

--- python/soap_client.py
+++ python/soap_client.py
@@ -2,4 +2,8 @@ from zeep import Client
 
 client = Client("http://localhost:8000")
 result = client.service.Saludar(nombre="Nicolas")
+sumar = client.service.Sumar(a=5, b=3)
+palindromo = client.service.CadenaPalindromo(cadena="Oruro")
 print(result)
+print(sumar)
+print(palindromo)
--- python/soap_server.py
+++ python/soap_server.py
@@ -6,6 +6,14 @@ def Saludar(nombre):
     return "!Hola, {}!".format(nombre)
 
 
+def Sumar(a, b):
+    return a + b
+
+
+def CadenaPalindromo(cadena):
+    return cadena == cadena[::-1]
+
+
 dispatcher = SoapDispatcher(
     "ejemplo-soap-server",
     location="http://localhost:8000/",
@@ -20,6 +28,20 @@ dispatcher.register_function(
     Saludar,
     returns={"saludo": str},
     args={"nombre": str},
+)
+
+dispatcher.register_function(
+    "Sumar",
+    Sumar,
+    returns={"resultado": int},
+    args={"a": int, "b": int},
+)
+
+dispatcher.register_function(
+    "CadenaPalindromo",
+    CadenaPalindromo,
+    returns={"resultado": bool},
+    args={"cadena": str},
 )
 
 server = HTTPServer(("0.0.0.0", 8000), SOAPHandler)
